62f183890b27fb038c2e0d1e880f32e6f769cdb1
StevOrc <steven.orcun2@gmail.com>
2021-03-03 23:33:39 +0100

    more setup for orders

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,10 +6,19 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be priveded");
   if (!process.env.MONGO_URI) throw new Error("MONGO URI must be provided");
+  if (!process.env.NATS_URL) throw new Error("NATS_URL must be provided");
+  if (!process.env.NATS_CLUSTER_ID)
+    throw new Error("NATS_CLUSTER_ID must be provided");
+  if (!process.env.NATS_CLIENT_ID)
+    throw new Error("NATS_CLIENT_ID must be provided");
   try {
     // Le 1er agrument doit être l'argument qu'on passe dans le fichier yaml pour NATS
     // Le 3eme agrument doit être le nom du service pour se connecter à NATS
-    await natsWrapper.connect("ticketing", "dchjqdc", "http://nats-srv:4222");
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on("close", () => {
       console.log("NATS connection close");
       process.exit();
