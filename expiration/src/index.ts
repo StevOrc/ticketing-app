1ae677bfdf97293e96ec1f6f11aaba216370311e
StevOrc <steven.orcun2@gmail.com>
2021-03-10 19:03:26 +0100

    adding queue for bull on redis

---
+++ expiration/src/index.ts
@@ -0,0 +1,34 @@
+import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  if (!process.env.NATS_URL) throw new Error("NATS_URL must be provided");
+  if (!process.env.NATS_CLUSTER_ID)
+    throw new Error("NATS_CLUSTER_ID must be provided");
+  if (!process.env.NATS_CLIENT_ID)
+    throw new Error("NATS_CLIENT_ID must be provided");
+  try {
+    // Le 1er agrument doit être l'argument qu'on passe dans le fichier yaml pour NATS
+    // Le 3eme agrument doit être le nom du service pour se connecter à NATS
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection close");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+  } catch (error) {
+    console.log("error");
+  }
+};
+
+start();
